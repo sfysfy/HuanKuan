3bd99c7237c519008a395baabd8defe0123eeff2
sfysfy <30351229+sfysfy@users.noreply.github.com>
2017-08-20 16:28:56 +0800

    Signed-off-by: sfysfy <30351229+sfysfy@users.noreply.github.com>

---
+++ app/src/main/java/com/repayment/money/entity/NewBillEntity.java
@@ -0,0 +1,65 @@
+package com.repayment.money.entity;
+
+/**
+ * Created by 11250 on 2017/8/20.
+ */
+
+public class NewBillEntity {
+
+    /**
+     * code : 0
+     * message : string
+     * resultObj : {}
+     * timestamp : 0
+     */
+
+    private int code;
+    private String message;
+    private ResultObjBean resultObj;
+    private long timestamp;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ResultObjBean getResultObj() {
+        return resultObj;
+    }
+
+    public void setResultObj(ResultObjBean resultObj) {
+        this.resultObj = resultObj;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public static class ResultObjBean {
+    }
+
+    @Override
+    public String toString() {
+        return "NewBillEntity{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", resultObj=" + resultObj +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
