0856eda677ae8608ae0694663c19e2c16bfd5546
MYH <1186265546@qq.com>
2017-08-21 20:25:14 +0800

    银行卡管理界面   绑定调试    库优化
    Signed-off-by: MYH <1186265546@qq.com>

--- app/src/main/java/com/repayment/money/ui/activity/LogincAtivity.java
+++ app/src/main/java/com/repayment/money/ui/activity/LogincAtivity.java
@@ -35,7 +35,7 @@ public class LogincAtivity extends BaseActivityWithNet<LoginEntity> {
 
     @Override
     protected int addRootView() {
-        return R.layout.login_main;
+        return R.layout.activity_login;
     }
 
   //  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
